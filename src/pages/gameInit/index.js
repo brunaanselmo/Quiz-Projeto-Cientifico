dcfde5512c66a82c3e11bc10175b8a49779150f2
brunaanselmo <b.anselmo@unesp.br>
2023-07-09 02:07:08 -0300

    fix: email

--- src/pages/gameInit/index.js
+++ src/pages/gameInit/index.js
@@ -7,10 +7,35 @@ import "./styles.css";
 
 export default function GameInit() {
 
+let emailUser = "";
+
 const rota = () => {
+  if (email){
   let route = "/Question01";
   return route;
+  }
 }
+
+const [email, setEmail] = useState('');
+
+const handleChange = (event) => {
+  setEmail(event.target.value);
+  console.log(email);
+};
+
+const handleClick = (event) => {
+  if (email){
+  localStorage.setItem("emailkey", JSON.stringify(email));
+  console.log(email);
+  emailUser = localStorage.getItem("emailkey");
+  console.log(emailUser);
+  rota();
+  } else {
+  alert("Por favor, insira um email válido");
+  }
+};
+
+
 
   return (
     <Container className="container">
@@ -22,13 +47,13 @@ const rota = () => {
             O objetivo do jogo é apenas avaliar os <br />seus conhecimentos sobre os tópicos
             <br />citados. </p>
           <div className="label-float">
-            <input id="email" type="email" required />
+            <input id="email" type="email" required value={email} onChange={handleChange}/>
             <label htmlFor="email">E-mail</label>
           </div>
         </div>
         <div className="next-question">
           <Link className="link" to={rota()}>
-            <button className="botao-start">Começar</button>
+            <button className="botao-start" onClick={handleClick}>Começar</button>
           </Link>
         </div>
       </div>
--- src/pages/references/index.js
+++ src/pages/references/index.js
@@ -1,13 +1,36 @@
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import Container from "@material-ui/core/Container";
 import Img from "../../components/img";
 import { FiFileText } from "react-icons/fi"
 import "./styles.css";
+
 export default function References() {
+
+
+const [feedback, setFeedback] = useState('');
+
+let feedbackUser = "";
+
+const handleChange = (event) => {
+  setFeedback(event.target.value);
+  console.log(feedback);
+};
 
 function handleClick(){
   window.location.assign("https://drive.google.com/file/d/1HrZ-4ZQfF7ZKPKMrB7rCOqdFCXgEhwSe/view?usp=sharing");
 }
+
+
+const handleClick2 = (event) => {
+  if (feedback){
+  localStorage.setItem("feedbackkey", JSON.stringify(feedback));
+  console.log(feedback);
+  feedbackUser = localStorage.getItem("feedbackkey");
+  console.log(feedbackUser);
+  alert("Comentário enviado. Obrigada!");
+  }
+};
 
   return (
     <Container className="container">
@@ -18,10 +41,10 @@ function handleClick(){
           <p className="references">Referências:</p>
           <FiFileText className="icone" style={{width:"80px", height:"80px", color: "rgb(89, 221, 89)", marginLeft: "17%", marginTop:"3%", cursor:"pointer"}} onClick={handleClick}/>
           <div className="label-float-references">
-            <input id="feedback" type="text" />
+            <input id="feedback" type="text" value={feedback} onChange={handleChange} />
             <label htmlFor="feedback">Comentários</label>
           </div>
-          <button className="botao-references">Enviar</button>
+          <button className="botao-references" onClick={handleClick2}>Enviar</button>
 
           </div>
           <div className="img">
